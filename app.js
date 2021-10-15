dfe2242f6b0e76bbd0d1fbf9264862e351050e5c
swastik <swastik7999@gmail.com>
2021-10-15 09:23:42 +0530

    :recycle: refactor consumer

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const modGuard = require('./components/modGuard');
 
-const moderationConsumer = require('./components/modGuard/consumers/modGuardConsumer1');
+const moderationConsumer1 = require('./components/modGuard/consumers/modGuardConsumer1');
+const moderationConsumer2 = require('./components/modGuard/consumers/modGuardConsumer2');
 
 const app = express();
 app.use(express.json());
 
 app.use(modGuard);
 
-moderationConsumer.startConsumer();
+moderationConsumer1.startConsumer();
+moderationConsumer2.startConsumer();
 
 module.exports = app;
